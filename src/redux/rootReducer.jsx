43b7e51f11005c372fd00f806c84a2a3882dc78b
Bipon Roy <biponroy400@gmail.com>
2024-03-03 01:34:13 +0600

    completed filters functionalities

--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -4,7 +4,7 @@ import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
     todos: todoReducer,
-    filter: filterReducer,
+    filters: filterReducer,
 });
 
 export default rootReducer;
